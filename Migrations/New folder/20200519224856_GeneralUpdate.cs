d86fc519ff7b83f5c42f0b17f78ae03b56091a78
muhammedogur88 <49583332+muhammedogur88@users.noreply.github.com>
2020-06-26 12:50:38 +0300

    Initial commit

---
+++ Migrations/New folder/20200519224856_GeneralUpdate.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class GeneralUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppinCartItems_Pies_PieId",
+                table: "ShoppinCartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppinCartItems",
+                table: "ShoppinCartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Pies");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppinCartItems",
+                newName: "ShoppingCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppinCartItems_PieId",
+                table: "ShoppingCartItems",
+                newName: "IX_ShoppingCartItems_PieId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ShoppingCartItems_Pies_PieId",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_ShoppingCartItems",
+                table: "ShoppingCartItems");
+
+            migrationBuilder.RenameTable(
+                name: "ShoppingCartItems",
+                newName: "ShoppinCartItems");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_ShoppingCartItems_PieId",
+                table: "ShoppinCartItems",
+                newName: "IX_ShoppinCartItems_PieId");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Pies",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_ShoppinCartItems",
+                table: "ShoppinCartItems",
+                column: "ShoppingCartItemId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ShoppinCartItems_Pies_PieId",
+                table: "ShoppinCartItems",
+                column: "PieId",
+                principalTable: "Pies",
+                principalColumn: "PieId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
